4dd8639b4a74b85b441f0f4671b8bc6f4d441239
Noë Snaterse <noe@wiredpea.com>
2014-09-29 16:01:41 +0200

    And the Drupal installation works too now.

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ A VM which has Drupal pre-installed
 1. Make sure you have Vagrant and Virtualbox installed.
 2. Run vagrant up
 
+##Credentials
+After the installation completes you can log into the site with the following credentials:
+Username: admin
+Password: admin
+
--- modules/drupal/manifests/init.pp
+++ modules/drupal/manifests/init.pp
@@ -5,22 +5,28 @@ class drupal {
     recurse => true,
     purge => true,
     force => true,
+    owner => "vagrant",
+    mode => 777,
     before => Class[apache]
   }
  
   exec { "drush dl drupal":
       cwd => '/www',
-      command => "drush dl drupal --drupal-project-rename=drupal",
+      command => "cd /www && drush dl drupal --drupal-project-rename=drupal",
+      provider => "shell",
+      user => "vagrant",
       require => [
-      Class["apache"],
-      Class["mysql"],
-      Package["drush"]
-    ]
+        Class["apache"],
+        Class["mysql"],
+        Package["drush"]
+      ]
   }
 
   exec { "drush si":
     cwd => '/www/drupal',
-    command => "drush si --db-url=mysql://root:root@127.0.0.1/drupal --account-pass=admin standard",
-    require => Exec["drush dl drupal"]
+    command => "cd /www/drupal && drush -y si --db-url=mysql://root:root@127.0.0.1/drupal --account-pass=admin standard",
+    provider => "shell",
+    require => Exec["drush dl drupal"],
+    user => "vagrant"
   }
 }
